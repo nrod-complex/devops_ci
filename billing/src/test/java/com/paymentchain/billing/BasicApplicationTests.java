91a69f6f4546c6621fb6055d8b64f3554b0cdeda
nrod-complex <rpv.practice@gmail.com>
2024-03-05 21:03:04 -0600

    nuevo cambio

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -12,7 +12,7 @@ public class BasicApplicationTests {
  
  @Test
  public void contextLoads() {
- 	    String meessage= "default message cambio test devops modificado";
+ 	    String meessage= "default message cambio test devops modificado 2";
  	    Assert.assertNotNull(meessage);
  }
  
