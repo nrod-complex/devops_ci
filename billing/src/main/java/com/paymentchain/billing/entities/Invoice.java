32ec71461ace05149a3618e05f664fc849c1f7c7
nrod-complex <rpv.practice@gmail.com>
2024-03-09 21:47:04 -0600

    Revert "nuevo cambio"
    This reverts commit 786650672aac20005bcceb3ab87b14856d21f70a.

--- billing/src/main/java/com/paymentchain/billing/entities/Invoice.java
+++ billing/src/main/java/com/paymentchain/billing/entities/Invoice.java
@@ -18,56 +18,13 @@ import lombok.Data;
  * @author sotobotero
  */
 @Entity
-//@Data
+@Data
 public class Invoice {
-    @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private long id;
-    private long customerId;
-    private String number;
-    private String detail;
-    private double amount;
-
-    public Invoice() {
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public long getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(long customerId) {
-        this.customerId = customerId;
-    }
-
-    public String getNumber() {
-        return number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
-    public String getDetail() {
-        return detail;
-    }
-
-    public void setDetail(String detail) {
-        this.detail = detail;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
+   @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+   private long id;
+   private long customerId;
+   private String number;
+   private String detail;
+   private double amount;  
 }
